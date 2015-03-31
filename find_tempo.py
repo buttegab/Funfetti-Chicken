4d1d2e33da217f484b30ddc9f633b89efa62ec8d
matthewruehle <matthew.ruehle@students.olin.edu>
2015-03-30 21:35:12 -0400

    updating api key

--- find_tempo.py
+++ find_tempo.py
@@ -1,11 +1,12 @@
 from pyechonest import config
-config.ECHO_NEST_API_KEY="DCPW87SS20PQ4RXDR"
+config.ECHO_NEST_API_KEY="NOLYZICKJ6J3JQ7LS"
 
 sad_foods = {1: 'tub of ice cream', 2:'bologna sandwich', 3: 'ramen noodles', 4: 'chicken broth', 5: 'hamburger helper', 6: 'frozen pizza'}
 def find_song(artist, song_name):
-	from pyechonest import song
+	from pyechonest import song # Re-importing ain't the most efficient, computation-wise.
 	rkp_results = song.search(artist = artist, title = song_name)
-	song = rkp_results[0]
+	song = rkp_results[0] # Overloading variables (e.g.: importing the "song" module and creating a "song" variable) isn't... ideal practice. Some packages, languages, etc. are less forgiving than Python.
+    print song.id
 	print song.audio_summary['tempo']
 	print song.audio_summary['mode']
 	print song.audio_summary['key']
