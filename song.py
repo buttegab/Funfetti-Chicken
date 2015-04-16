304822365cff75d6123c2a9841d44a671cee8a09
Jaime Grau <JGrau@jaimes-mbp.olin.edu>
2015-04-15 20:32:20 -0400

    Pattern Working

---
+++ song.py
@@ -0,0 +1,70 @@
+"""
+Funfetti Chicken
+SoftDes Spring 2015
+
+"""
+from pattern.en import *
+import requests
+from pyechonest import config
+config.ECHO_NEST_API_KEY="DCPW87SS20PQ4RXDR"
+from pyechonest import song
+
+def get_json(url):
+    """
+    Given a properly formatted URL for a JSON web API request, return
+    a Python JSON object containing the response to that request.
+    """
+    f = urllib2.urlopen(url)
+    response_text = f.read()
+    response_data = json.loads(response_text)
+    return response_data
+
+# User inputs.
+#artist = raw_input("Please enter an artist: ")
+#title = raw_input("Please enter an title: ")
+artist='coldplay'
+title='clocks'
+
+# Song search
+rkp_results = song.search(artist=artist,title=title)
+song = rkp_results[0]
+
+#Get lyrics
+url = 'http://api.lyricsnmusic.com/songs?api_key=[5358b25688164e6c2f771954f17460&q]='+artist+'%20'+title
+r = requests.get(url)
+r_text = r.text
+
+#Clean lyrics
+r_text_pythonic = r_text.replace('false', 'False')
+rtp = r_text_pythonic.replace('true', 'True')
+rtp2 = rtp.replace('null', 'None')
+r_text_as_list = eval(rtp2)
+
+test_dictionary = r_text_as_list[0]
+
+p1 = test_dictionary['snippet']
+p2 = test_dictionary['context']
+
+test_dictionary = r_text_as_list[2]
+p3 = test_dictionary['snippet']
+
+#Check food dictionary
+#print p1+p2+''+p3
+
+#Analize lyrics sentiments
+sentiment = sentiment(p1+p2+''+p3)
+print 'lyrics sentiment: ',sentiment
+
+#Exact information about the song
+#print song.artist_location
+#print 'tempo:',song.audio_summary['tempo']
+#print 'duration:',song.audio_summary['duration']
+print 'energy:',song.audio_summary['energy']
+#print 'key:',song.audio_summary['key']
+print 'liveness:',song.audio_summary['liveness']
+#print 'loudness:',song.audio_summary['loudness']
+#print 'mode:',song.audio_summary['mode']
+#print 'acousticness:' ,song.audio_summary['acousticness']
+#print 'speechiness:',song.audio_summary['speechiness']
+#print 'time_signature:',song.audio_summary['time_signature']
+print 'danceability:',song.audio_summary['danceability']
