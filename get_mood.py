8ec4ca5662793af11c08ab6b24589258c4491e0f
Aubrey Dority <aubrey.dority@students.olin.edu>
2015-04-21 15:40:08 -0400

    testing the API to get it to spit out mood in the right form

--- get_mood.py
+++ get_mood.py
@@ -1,14 +1,14 @@
 import json
 import urllib2 
+import pygn
+userID = '27678235334765580-1FB57EBF64483B1937D128ACEB314A68'
 
-def get_mood():
-	title = 'creep'
-	url = "http://api.rovicorp.com/data/v1.1/song/info?track=yellow&include=moods&country=US&language=en&format=json&apikey=g6mj4hxhrs5na3827sj6bygx&sig=42e988e601a1f909d7af968a007337a5"
-	#'http://www.jamrockentertainment.com/billboard-music-top-100-songs-listed-by-year/top-100-songs-1950.html'
-	#str('http://developer.echonest.com/api/v4/song/search?api_key=DCPW87SS20PQ4RXDR&format=json&results=1&artist=radiohead&title='+title)
-	#"http://api.rovicorp.com/data/v1.1/song/info?track=yellow&country=US&language=en&format=json&apikey=a4779rmf2wq6h9w4af6hryzh&sig=890aca71f1beff8562793e1c170ce4c2"
-	data = json.load(urllib2.urlopen(url))
-	return data
+def get_mood(artist, name):
+	artist = str(artist)
+	title = str(name)
+	data = pygn.search(clientID='10189056-547D2643844BF1E2B9E85BD85773AEFE', userID=userID, artist=artist, track=title)
+	return data['mood']
 	#['response']['songs'][0]['id']
 
-print get_mood()
+print userID
+print get_mood('bob dylan', 'like a rolling stone')
