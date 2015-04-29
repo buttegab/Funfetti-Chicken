86117503f8c22db80d81f8bf18e2328e2f8320d9
matthewruehle <matthew.ruehle@students.olin.edu>
2015-04-29 01:13:15 -0400

    work in progress still

--- bigger_database_creator.py
+++ bigger_database_creator.py
@@ -8,15 +8,21 @@ from get_a_bunch_of_songs import condense_list
 import time
 from os.path import exists
 
-f = open('billboard_song_list.txt', 'r+')
+h = open('billboard_song_list.txt', 'r+')
 g = open('more_billboard_songs.txt', 'r+')
+f = open('yet_more_billboard_songs.txt', 'r+')
 
-recent_songs = pickle.load(f)
-less_recent_songs = pickle.load(g)
-recent_songs.extend(less_recent_songs)
+tons_of_songs = pickle.load(f)
+full_list = tons_of_songs
+# recent_songs = pickle.load(h)
+# less_recent_songs = pickle.load(g)
+# tons_of_songs.extend(recent_songs)
+# tons_of_songs.extend(less_recent_songs)
 
-full_list = condense_list(recent_songs)
+# full_list = condense_list(tons_of_songs)
 
+# recent_songs.extend(less_recent_songs)
+# full_list = condense_list(recent_songs)
 
 def add_to_db(artist, song, db):
     """
@@ -39,21 +45,22 @@ def add_to_db(artist, song, db):
         pickle.dump(song_list, f)
 
 
-wait_index = 0 # to keep track of api rate limits.
+wait_index = 4865 # to keep track of api rate limits.
 
 
-for (x, y) in full_list:
+for (x, y) in full_list[4865:]:
     try:
-        add_to_db(x, y, 'full_database.txt')
+        add_to_db(x, y, 'fifty_seven_years_database.txt')
     except:
-        time.sleep(60)
+        print 'failed: ' + str(x) + ':' + str(y) + '. Retrying.'
+        time.sleep(10)
         try:
-            add_to_db(x, y, 'full_database.txt')
-        except:
-            pass
+            add_to_db(x, y, 'fifty_seven_years_database.txt')
+        except: 
+            print 'failed: ' + str(x) +  ':' + str(y) + '. Skipping.'
     wait_index += 1
 
     print wait_index
 
-    if wait_index%40 == 0:
-        time.sleep(70)
+    if wait_index%100 == 0:
+        time.sleep(61)
