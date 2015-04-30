1a0dd922e8a49ccf83f03cac8ebb8c30de46d4dd
buttegab <gabriel.butterick@students.olin.edu>
2015-04-29 21:05:39 -0400

    Changed the code to accept a song and artist, gathers all the data but the lyric sentiment because its buggy, and returns a mood if the accuracy is greater than 30%

--- song_db_to_ml_wrapper.py
+++ song_db_to_ml_wrapper.py
@@ -8,6 +8,25 @@ import make_echonest_database
 import pickle
 from sklearn import svm
 import pprint
+import requests
+from pyechonest import config
+config.ECHO_NEST_API_KEY="DCPW87SS20PQ4RXDR"
+from pattern.en import *
+
+
+def get_data(artist, title):
+    from pyechonest import song
+    parameters = []
+    #artist = 'coldplay'
+    #title = 'clocks'
+    rkp_results = song.search(artist=artist,title=title)
+    song = rkp_results[0]
+    song.audio_summary['sentiment'] = 0
+    parameters.append([song.audio_summary[k] for k in ['tempo', 'mode', 'key', 'danceability', 'acousticness', 'speechiness', 'loudness', 'energy', 'sentiment']])
+    return parameters
+
+
+
 
 f = open('full_database.txt', 'r+')
 
@@ -26,12 +45,13 @@ for song in song_data_list:
 
 parameters = [] # train x
 moodout = [] # train y
+currentSong = [] #Unknown song
 
 mood_to_small_integer = {'Empowering': 0, 'Yearning': 1, "Defiant": 2, "Sensual": 3, "Cool": 4, "Excited": 5, "Energizing": 6, "Urgent": 7, "Rowdy": 8, "Brooding": 9}
 
 for i in usable_songs:
     # print i.name
-    parameters.append([i.parameter_dict[k] for k in ['tempo', 'mode', 'key', 'sentiment', 'danceability', 'acousticness', 'speechiness', 'loudness', 'energy']])
+    parameters.append([i.parameter_dict[k] for k in ['tempo', 'mode', 'key', 'danceability', 'acousticness', 'speechiness', 'loudness', 'energy','sentiment']])
     moodout.append(mood_to_small_integer[i.mood1_text])
 
 # pprint.pprint(zip(moodout, parameters))
@@ -49,13 +69,17 @@ accuracy = 0.0
 # print moodout[173]
 
 for i in range(len(parameters)):
-    print i
+    #print i
     prediction = lin_clf.predict(parameters[i])
-    print("Predicted: {}; Actual: {}".format(prediction, moodout[i]))
+    #print("Predicted: {}; Actual: {}".format(prediction, moodout[i]))
     accuracy += float(prediction == moodout[i])/len(parameters)
 
 accuracy = accuracy * 100
-if accuracy >= 30:
-	print("Accuracy: {}%".format(accuracy))
-else:
+if accuracy <= 30:
 	print("Accuracy too low, please run again")
+
+songdata = get_data('coldplay', 'clocks')
+prediction = lin_clf.predict(songdata)
+if accuracy>= 30:
+    print("Mood: {}".format(prediction))
+    
