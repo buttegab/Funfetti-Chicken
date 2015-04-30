d8447bb292e21f603d36c2ed189a3662894889c9
buttegab <gabriel.butterick@students.olin.edu>
2015-04-30 17:37:16 -0400

    so much machine learning got added, now it gives a lot more information about what is going on, the data is much more refined, the accuracy is higher, there is a training and test data set, and the ending data is normalized

--- song_db_to_ml_wrapper.py
+++ song_db_to_ml_wrapper.py
@@ -7,6 +7,7 @@ prediction code, sometimes multiple runs are necessary in order to return a reas
 import make_echonest_database
 import pickle
 from sklearn import svm
+from sklearn.metrics import confusion_matrix
 import pprint
 import requests
 from pyechonest import config
@@ -37,7 +38,7 @@ song_data_list = song_data_list[1:] # gets rid of the first entry, which is kind
 
 usable_songs = []
 
-moods_of_interest = ['Empowering','Yearning', 'Defiant', 'Sensual', 'Cool', 'Excited', 'Energizing', 'Urgent']
+moods_of_interest = ['Yearning', 'Brooding', 'Excited', 'Empowering']
 
 for song in song_data_list:
     if song.mood1 != 'N/A' and song.id != 'N/A' and song.lyrics != '' and song.mood1_text in moods_of_interest and song.parameter_dict['speechiness'] != None:
@@ -48,12 +49,16 @@ parameters = [] # train x
 moodout = [] # train y
 currentSong = [] #Unknown song
 
-mood_to_small_integer = {'Empowering': 0, 'Yearning': 1, "Defiant": 2, "Sensual": 3, "Cool": 4, "Excited": 5, "Energizing": 6, "Urgent": 7, "Rowdy": 8, "Brooding": 9}
+mood_to_small_integer = {'Yearning': 0, "Brooding": 1, "Excited": 2, "Empowering": 3}
 
 for i in usable_songs:
     # print i.name
     parameters.append([i.parameter_dict[k] for k in ['tempo', 'mode', 'key', 'danceability', 'acousticness', 'speechiness', 'loudness', 'energy','sentiment']])
     moodout.append(mood_to_small_integer[i.mood1_text])
+
+from sklearn.preprocessing import scale
+
+parameters = scale(parameters)
 
 # pprint.pprint(zip(moodout, parameters))
 
@@ -62,22 +67,50 @@ for i in usable_songs:
 #         print i
 #         print 'is not an int'
 
-lin_clf = svm.LinearSVC()
-lin_clf.fit(parameters, moodout)
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
 
-accuracy = 0.0
+
+parameters_train, parameters_test, moodout_train, moodout_test  = train_test_split(parameters, moodout, train_size=0.8)
+
+val_counts = [moodout_train.count(0), moodout_train.count(1), moodout_train.count(2), moodout_train.count(3)]
+print val_counts
+
+#lin_clf = svm.LinearSVC()
+lin_clf = svm.SVC(kernel='linear',C=100000)
+#lin_clf = LogisticRegression()
+print len(parameters_train), len(parameters_test), len(parameters)
+total_weight = sum([1.0/val_counts[v] for v in moodout_train])
+print parameters[0]
+lin_clf.fit(parameters_train, moodout_train, sample_weight=[1.0/val_counts[v]/total_weight for v in moodout_train])
+print lin_clf
+
 # print parameters[173]
 # print moodout[173]
-
-for i in range(len(parameters)):
+predictList = []
+accuracy = 0.0
+for i in range(len(parameters_test)):
     #print i
-    prediction = lin_clf.predict(parameters[i])
+    prediction = lin_clf.predict(parameters_test[i])
+    predictList.append(prediction)
     #print("Predicted: {}; Actual: {}".format(prediction, moodout[i]))
-    accuracy += float(prediction == moodout[i])/len(parameters)
+    accuracy += float(prediction == moodout_test[i])/len(parameters_test)
+confusion = confusion_matrix(moodout_test, predictList)
+print confusion
+import numpy as np
+print np.sum(confusion,axis=1)
+list2 = np.sum(confusion,axis=1)
+for sumn in range(0,4):
+    print(float(confusion[sumn][sumn])/float(list2[sumn]))
+    #print(confusion[sumn][sumn])
+    #print(list2[sumn])
 
 accuracy = accuracy * 100
+print accuracy
 if accuracy <= 30:
 	print("Accuracy too low, please run again")
+
+#print(confusion_matrix(moodout, prediction))
 
 def get_mood(artist,track):
     songdata = get_data(artist,track)
@@ -93,6 +126,8 @@ def show_food(artist,track):
     mood = get_mood(artist,track)
     text = g.te(width=25, height=1)
     text.insert(END,foods[mood])
+
+
 
 g = Gui()
 g.title('Funfetti Chicken')
